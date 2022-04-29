89ce63719a0ed92cd128fd29ea79a47fb868659b
fayçal ben <bensassi.faysel@gmail.com>
2022-04-29 01:16:01 +0000

    Main.py

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ import authorize as auth
 from chatbot import main
 status = auth.AOSauth()
 aos.Console.clear
-aos.Console.warn("OWNED BY [VIR]\nVisions Into Reality")
+aos.Console.warn("3ème année finance Compétences digitales (Programmation Python)")
 aos.Misc.help() #TODO: Ansi, reference as file not PIP for certain modules (user-requested)
 aos.Console.commentary("Admin Operations System")
 aos.Console.type("Version 1.0\n")
 aos.Misc.newline()
-aos.Console.type("Here are available CDNs for installing AOS as well as docs and our discord server: ")
-aos.Console.type("\nDeadWither's CDN for installation: https://aos-installer.deadwither.repl.co/")
-aos.Console.type("\n[NOT UPDATED] Mkcodes CDN w/ markdown file from deadwither: https://aos-docs.mkcodes.repl.co/.\n")
-aos.Console.warn("DISCORD SERVER: https://discord.gg/5p2ej78b6K")
+aos.Console.type("Project de Mohamed Aziz Ben Sassi ")
+aos.Console.type("\Institut Supérieur de Gestion de Tunis /")
+aos.Console.type("\Enseignante: Mme. Imen Oueslati\n")
 aos.Misc.newline()
 main()
